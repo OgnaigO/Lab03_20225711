b5ba8ca7b6c50f332d92031e0fed42b1c0294d9a
OgnaigO <fery101112@gmail.com>
2024-11-25 22:59:52 +0700

    Bai5

--- Lab03/CodeJava/AimsProject/src/DigitalVideoDisc.java
+++ Lab03/CodeJava/AimsProject/src/DigitalVideoDisc.java
@@ -1,45 +1,75 @@
-
 public class DigitalVideoDisc {
-	public DigitalVideoDisc(String title, String category, String directory, int length, float cost) {
-		super();
-		this.title = title;
-		this.category = category;
-		this.directory = directory;
-		this.length = length;
-		this.cost = cost;
-	}
-	public DigitalVideoDisc(String title, String category, float cost) {
-		super();
-		this.title = title;
-		this.category = category;
-		this.cost = cost;
-	}
-	public DigitalVideoDisc(String title) {
-		super();
-		this.title = title;
-	}
-	private String title;
-	private String category;
-	private String directory;
-	private int length;
-	private float cost;
-	public String getTitle() {
-		return title;
-	}
-	public String getCategory() {
-		return category;
-	}
-	public String getDirectory() {
-		return directory;
-	}
-	public int getLength() {
-		return length;
-	}
-	public float getCost() {
-		return cost;
-	}
-	public void setTitle(String title2) {
-		// TODO Auto-generated method stub
-		
-	}
+    private static int nbDigitalVideoDiscs = 0;
+
+    private int id;
+
+    private String title;
+    private String category;
+    private String directory;
+    private int length;
+    private float cost;
+
+    public DigitalVideoDisc(String title, String category, String directory, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.directory = directory;
+        this.length = length;
+        this.cost = cost;
+
+        // Tăng số lượng DVD và gán ID duy nhất
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+    }
+
+    // Constructor với 3 tham số
+    public DigitalVideoDisc(String title, String category, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+    }
+
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+    }
+
+    // Getter cho ID
+    public int getId() {
+        return id;
+    }
+
+    // Getter cho tổng số DVD đã được tạo
+    public static int getNbDigitalVideoDiscs() {
+        return nbDigitalVideoDiscs;
+    }
+
+    // Getters cho các thuộc tính khác
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDirectory() {
+        return directory;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    // Setter cho title
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
